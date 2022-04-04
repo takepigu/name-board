b36cafc427f55892024a02936ed6a05a358826ef
takeumi.ishihara <takeumiishihara@gmail.com>
2022-04-04 07:45:41 +0000

    add seeder

---
+++ database/seeds/NamesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class NamesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i<= 100; $i++) {
+            DB::table('names')->insert([
+                'title' => 'test title' . $i,
+                'content' => 'test content' . $i
+                ]);
+        }
+    }
+}
